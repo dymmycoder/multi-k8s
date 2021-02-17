11f4ea8d42f7d606f1f45e1cea0e3e17a977983d
dymmycoder <shengcor@gmail.com>
2021-02-16 23:46:54 -0500

    added deployment scripts.

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t shengcer/multi-client:latest -t shengcer/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t shengcer/multi-server:latest -t shengcer/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t shengcer/multi-worker:latest -t shengcer/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push shengcer/multi-client:latest
+docker push shengcer/multi-server:latest
+docker push shengcer/multi-worker:latest
+docker push shengcer/multi-client:$SHA
+docker push shengcer/multi-server:$SHA
+docker push shengcer/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=shengcer/multi-server:$SHA
+kubectl set image deployments/client-deployment client=shengcer/multi-client:$SHA
+kubectl set image deployments/worker-deployments worker=shengcer/multi-worker:$SHA
